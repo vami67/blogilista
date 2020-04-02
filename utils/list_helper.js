b18c2e3b5b5cacff115c37711ce2a7987f462824
Mika Vahlberg <mika.vahlberg@iki.fi>
2020-04-02 14:04:46 +0300

    4.5*: apufunktioita ja yksikkötestejä, step3

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -14,7 +14,16 @@ const totalLikes = (blogs) => {
     return blogs.reduce((sum, blog) => sum + blog.likes, 0)
 }
 
+
+const favoriteBlog = (blogs) => {
+
+    const sortedBlogs = blogs.sort(function (a, b) { return a.likes - b.likes })
+
+    
+    return sortedBlogs[sortedBlogs.length-1]
+}
 module.exports = {
     dummy,
-    totalLikes
+    totalLikes,
+    favoriteBlog
 }
