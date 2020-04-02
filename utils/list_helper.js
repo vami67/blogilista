16dd8cc75b081e5b58c9ec9941a0f22f175a3487
Mika Vahlberg <mika.vahlberg@iki.fi>
2020-04-02 12:41:35 +0300

    4.4: apufunktioita ja yksikkötestejä, step2

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -2,6 +2,19 @@ const dummy = (blogs) => {
     return 1
 }
 
+
+const totalLikes = (blogs) => {
+
+    const reducer = (sum, item) => {
+        return sum + item
+    }
+
+    //blogs.reduce((sum,item)=>sum+item)
+
+    return blogs.reduce((sum, blog) => sum + blog.likes, 0)
+}
+
 module.exports = {
-    dummy
+    dummy,
+    totalLikes
 }
