a75a2382d8d1ee9242f7abf2def5f55f9df7def4
Mika Vahlberg <mika.vahlberg@iki.fi>
2020-04-07 11:50:51 +0300

    4.15: blogilistan laajennus, step4

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ const blogsRouter = require('./controllers/blogs')
 const logger = require('./utils/logger')
 const mongoose = require('mongoose')
 const middleware = require('./utils/middleware')
+const usersRouter = require('./controllers/users')
+
 
 logger.info('connecting to', config.MONGODB_URI)
 
@@ -22,6 +24,7 @@ app.use(express.json())
 app.use(middleware.requestLogger)
 
 app.use('/api/blogs', blogsRouter)
+app.use('/api/users', usersRouter)
 
 app.use(middleware.unknownEndpoint)
 app.use(middleware.errorHandler)
