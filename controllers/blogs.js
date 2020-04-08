6e4fc380b6976fc046a60fbb1b14ff320f51e44d
Mika Vahlberg <mika.vahlberg@iki.fi>
2020-04-08 10:57:01 +0300

    4.17: blogilistan laajennus, step6

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -1,13 +1,25 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
+const User = require('../models/user')
 
 blogsRouter.get('/', async (request, response) => {
-  const blogs = await Blog.find({})
+  const blogs = await Blog.find({}).populate('user')
   response.json(blogs.map(blog => blog.toJSON()))
 })
 
 blogsRouter.post('/', async (request, response) => {
-  const blog = new Blog(request.body)
+
+  const body = request.body
+  const users = await User.find({})
+  
+  const blog = new Blog({
+    title:body.title,
+    author:body.author,
+    url:body.url,
+    likes:body.likes,
+    user: users[0]._id
+  })
+
   const savedBlog = await blog.save()
   response.json(savedBlog.toJSON())
 })
