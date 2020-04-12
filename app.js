f601a79c75bc24a40ae6f245deeed09bf40c908e
Mika Vahlberg <mika.vahlberg@iki.fi>
2020-04-12 12:02:59 +0300

    4.19: blogilistan laajennus, step8

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ const loginRouter = require('./controllers/login')
 
 logger.info('connecting to', config.MONGODB_URI)
 
-mongoose.connect(config.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(config.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex:true})
     .then(() => {
         logger.info('connected to MongoDB')
     })
